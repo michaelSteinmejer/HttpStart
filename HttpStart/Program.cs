689e096ace9158d6428ffdee2178b7f1eb02f991
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2018-12-26 12:56:07 +0100

    lol

---
+++ HttpStart/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace HttpStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dette er ip adressen
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            // Lytter på ip adressen og port nummer 7000
+            TcpListener serverSocket = new TcpListener(ip, 7000);
+            //starter server
+            serverSocket.Start();
+            Console.WriteLine("Server started!!");
+
+            while (true)
+            {
+                // acceptere af der en ny forbindelse og sætter den til at være eb tcp client.
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Server Activated!!");
+
+                TCPService service = new TCPService(connectionSocket);
+
+                Task.Factory.StartNew(service.doit);
+            };
+        }
+    }
+}
---
+++ HttpStart/TCPService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace HttpStart
+{
+    class TCPService
+    {
+        public TcpClient connectionSocket { get; set; }
+        public string http { get; set; } = "HTTP/1.1 200 OK\r\n";
+        public string Content { get; set; } = "Content-Type: text/html\r\n";
+        public string Close { get; set; } = "Connection: close\r\n";
+        public string RN { get; set; } = "\r\n";
+
+
+        public TCPService(TcpClient connection)
+        {
+            this.connectionSocket = connection;
+
+        }
+
+        public void doit()
+        {
+            while (true)
+            {
+                Stream ns = connectionSocket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+
+                sw.AutoFlush = true;
+                
+
+                while (true)
+                {
+
+                    string answer = "";
+                    string message = sr.ReadLine();
+                    sw.WriteLine(answer);
+
+                    if (message.ToLower() == "stop")
+                    {
+
+                        answer = "connection closed";
+                        sw.WriteLine(answer);
+                        ns.Close();
+                        sr.Close();
+                        sw.Close();
+
+
+                    }
+
+                    Console.WriteLine(message);
+                    
+                    if (message.Contains("GET") && message.Contains("HTTP/1.1"))
+                    {
+                        var s = message.Split(' ', ' ')[0 + 1];
+                       // answer = http + Content + Close + RN + "Hello Client" + RN;
+
+                        var page = File.ReadAllText(Environment.CurrentDirectory + "/somefile.html");
+
+                        answer = http + Content + Close + RN + page + RN;
+                        sw.WriteLine(answer);
+                    }
+                }
+            }
+        }
+    }
+}
